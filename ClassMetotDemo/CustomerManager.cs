225c3153b06eb103f0f906111fc779181b8fe1d6
ilker yasar <ilker-579@hotmail.com>
2021-04-29 15:58:28 +0300

    29.04.2021

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    class CustomerManager
+    {
+        public void AddCustomer(Customer customer)
+        {
+            Console.WriteLine("Customer Added. " +" "+"TCNo: "+" "+customer.TCNo+", "+"Name and Surname: "+" "+customer.Name+" "+customer.Surname);
+        }
+
+        public void ListCustomer(Customer customer)
+        {
+            Console.WriteLine(customer.Id);
+            Console.WriteLine(customer.Name);
+            Console.WriteLine(customer.Surname);
+            Console.WriteLine(customer.TCNo);
+        }
+
+        public void DeleteCustomer(Customer customer)
+        {
+            Console.WriteLine("Name:"+" " +customer.Name+ " "+"**** Deleted");
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer();
+            customer1.Name = "İlker";
+            customer1.Surname = "Yaşar";
+            customer1.TCNo = 12354621458;
+
+            Customer customer2 = new Customer();
+            customer2.Name = "Ayşe";
+            customer2.Surname = "Yazıcı";
+            customer2.TCNo = 12345687421;
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.AddCustomer(customer2);
+            customerManager.ListCustomer(customer1);
+            customerManager.DeleteCustomer(customer2);
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
